350b2d793d098b3c250f9c56c38e629ee1958fcc
Jasonhunter127 <jasonhunter127@gmail.com>
2018-02-26 14:51:32 -0500

    Module 4
    -part 1
    -part 2
    
    Also added comments to module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,6 +13,7 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
+            // Cannot calculate letter grade for less than 5 students
             if (Students.Count < 5)
             {
                 throw new InvalidOperationException();
@@ -24,16 +25,46 @@ namespace GradeBook.GradeBooks
                                 .Select(e => e.AverageGrade)
                                 .ToList();
 
+            // The threshold acts as an index in the list
             if (grades[threshold - 1] <= averageGrade)
-                return 'A';
+                return 'A';     // Top 20% of students
             else if (grades[(threshold * 2) - 1] <= averageGrade)
-                return 'B';
+                return 'B';     // Top 40% of students
             else if (grades[(threshold * 3) - 1] <= averageGrade)
-                return 'C';
+                return 'C';     // Top 60% of students
             else if (grades[(threshold * 4) - 1] <= averageGrade)
-                return 'D';
+                return 'D';     //Top 80% of students
 
+            // Return base case
             return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            // Cannot calculate statistics for less than 5 students
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students " +
+                                    "with grades in order to properly calculate a " +
+                                    "studen't overall grade.");
+            }
+
+            // Call base method
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            // Cannot calculate statistics for less than 5 students
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with " +
+                                    "grades in order to properly calculate a student's " +
+                                    "overall grade.");
+            }
+
+            // Call base method
+            base.CalculateStudentStatistics(name);
         }
     }
 }
